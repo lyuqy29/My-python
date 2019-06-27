11db107e7b9a0d136b1c3bbfa463350c58c36002
Quyang <quyang@Quyangs-MacBook-Pro.local>
2019-06-27 23:53:01 +0800

    update content

--- chapter_2.py
+++ chapter_2.py
@@ -1,9 +1,18 @@
-data=[1,5,-3,-2,6,8,0,9]
+#coding=utf8
+# test 1
+# 随机数字 -10 - 10 数量: 10
+from random import randint
 
-res=[]
+data = [randint(-10,10) for _ in xrange(10) ]
 
-for x in data:
-    if x >= 0 :
-        res.append(x)
+#print filter(lambda x: x>= 0 , data)
+#打印出大于0的数字
+print  [x for x in data if x >= 0]
 
-print res
+# test 2
+
+#字典解析
+d = {x: randint(60, 100) for x in xrange(1,21)}
+
+print {k:v for k,v in d.iteritems() if v >90}
+
